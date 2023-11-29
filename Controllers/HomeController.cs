8b9fde1482e97b93be4584f4172e5d00defe7cd1
tanch0 <tanchohanglimbu2@gmail.com>
2023-11-29 08:02:08 +0545

    Template

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TempECommerce.Models;
+
+namespace TempECommerce.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult ShoppingCart()
+    {
+        return View();
+    }
+
+    public IActionResult BlogDetail()
+    {
+        return View("BlogDetail");
+    }
+
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    public IActionResult Product()
+    {
+        return View();
+    }
+
+    public IActionResult Blog()
+    {
+        return View();
+    }
+    public IActionResult Contact()
+    {
+        return View();
+    }
+
+
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
